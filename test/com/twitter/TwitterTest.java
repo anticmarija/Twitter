d1811b5cfcb96af8ab5b664afd41d738bb3f5ac0
majaantic3113 <majaantic994@gmail.com>
2015-04-03 15:48:21 +0200

    Testirana metoda vratiSvePoruke, ali test pao jer metoda vraca null, a treba da vrati litu poruke.
    U metodi unesi, greska u kodu, metoda postavlja String "korisnik" za
    korisnika a ne ulazni parametar korisnik.
    
    U metodi vratiPoruke, greska u kodu jer se brojac uvecava za 1 pa prvi
    element u nizu ostaje null, a trebalo bi da vrati prvu poruku iz liste
    koja sadrzi dati tag.

---
+++ test/com/twitter/TwitterTest.java
@@ -0,0 +1,142 @@
+/**
+ * 
+ */
+package com.twitter;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.twitter.poruke.TwitterPoruka;
+
+/**
+ * Testiranje klase Twitter
+ * @author marijaantic
+ * @version 1.0
+ *
+ */
+public class TwitterTest {
+	
+	private Twitter t;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	
+		t = new Twitter();
+	
+	}
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		
+		t = null;
+	}
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#vratiSvePoruke()}.
+	 */
+	@Test
+	public void testVratiSvePoruke() {
+		
+		TwitterPoruka tp1 = new TwitterPoruka();
+		tp1.setKorisnik("Mara");
+		tp1.setPoruka("ovo je marin tvit");
+		
+		TwitterPoruka tp2 = new TwitterPoruka();
+		tp2.setKorisnik("Pera");
+		tp2.setPoruka("ovo je perin tvit");
+		
+		LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+		poruke.add(tp1);
+		poruke.add(tp2);
+		
+		
+		t.vratiSvePoruke();
+		
+		assertEquals(poruke, t.vratiSvePoruke());
+		
+		
+	}
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#unesi(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testUnesi() {
+		
+		TwitterPoruka tp = new TwitterPoruka();
+		
+		tp.setKorisnik("Mara");
+		tp.setPoruka("ovo je marin tvit #tweet");
+		
+		t.unesi("Mara", "ovo je marin tvit #tweet");
+		
+		assertEquals(1, t.vratiSvePoruke().size());
+		assertEquals(tp, t.vratiSvePoruke().get(0));
+	}
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#vratiPoruke(int, java.lang.String)}.
+	 */
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeTagNull() {
+		
+		t.vratiPoruke(5, null);
+		
+	}
+	
+	/**
+	 * Test method for {@link com.twitter.Twitter#vratiPoruke(int, java.lang.String)}.
+	 */
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeTagPrazan() {
+		
+		t.vratiPoruke(5, "");
+		
+	}
+	
+
+	/**
+	 * Test method for {@link com.twitter.Twitter#vratiPoruke(int, java.lang.String)}.
+	 */
+	@Test 
+	public void testVratiPoruke() {
+		
+		TwitterPoruka tp1 = new TwitterPoruka();
+		tp1.setKorisnik("Mara");
+		tp1.setPoruka("ovo je marin tvit #tweet");
+		
+		TwitterPoruka tp2 = new TwitterPoruka();
+		tp2.setKorisnik("Pera");
+		tp2.setPoruka("ovo je perin tvit #tweet");
+		
+		TwitterPoruka tp3 = new TwitterPoruka();
+		tp3.setKorisnik("Pera");
+		tp3.setPoruka("ovo je perin tvit #pera");
+		
+		LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+		poruke.add(tp1);
+		poruke.add(tp2);
+		poruke.add(tp3);
+		
+		TwitterPoruka[] rezultat = t.vratiPoruke(2, "#tweet");
+		
+		TwitterPoruka[] tweet = new TwitterPoruka[2];
+		tweet[0] = tp1;
+		tweet[1] = tp2;
+		
+		assertEquals (tweet.length, rezultat.length);
+		assertEquals (tweet[0], rezultat[0]);
+		
+		
+	}
+}
